e01bc291c60603129d73150fe9aae3637700714c
Liz Love <love.elizabeth1989@gmail.com>
2020-07-15 21:51:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,14 +5,14 @@ def load_library(library)
   new_hash = {
    "get_meaning" => {},
    "get_emoticon" => {}
- }
+  }
  library.each do |meaning, translation|
    english = translation[0]
    japanese = translation[1]
    new_hash["get_meaning"][japanese] = meaning
    new_hash["get_emoticon"][english] = japanese
  end
-  return new_hash
+  new_hash
 end
 
 

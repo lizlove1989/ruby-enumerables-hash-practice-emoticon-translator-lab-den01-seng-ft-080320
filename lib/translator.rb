b29a7398f834645f63930430922a6c0067090d6e
Liz Love <love.elizabeth1989@gmail.com>
2020-07-15 21:59:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,34 +1,37 @@
-require 'yaml'
-require 'pry'
+require "yaml"
+require "pry"
 
-def load_library(path)
-  emoticons = YAML.load_file(path)
-  emoticon_hash = Hash.new
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
 
-  emoticon_hash["get_emoticon"] = Hash.new
-  emoticon_hash["get_meaning"] = Hash.new
+  return_hash = {
+    "get_meaning" => {},
+    "get_emoticon" => {},
+  }
 
-  emoticons.each do |english_word, emoticon_set|
-    emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
-    emoticon_hash["get_meaning"][emoticon_set.last] = english_word
+  emoticons.each do |meaning, translations|
+    return_hash["get_meaning"][translations[1]] = meaning
+    return_hash["get_emoticon"][translations[0]] = translations[1]
   end
-  emoticon_hash
+  return_hash
 end
 
-def get_japanese_emoticon(path, emoticon)
-  emoticon_hash = load_library(path)
-  result = emoticon_hash["get_emoticon"][emoticon]
-  if result == nil
-    result = "Sorry, that emoticon was not found" 
+def get_japanese_emoticon(file_path, emoticon)
+  emoticons = load_library(file_path)
+  emoticons["get_emoticon"].each do |english_emoticon, japanese_emoticon|
+    if emoticon == english_emoticon
+      return japanese_emoticon
+    end
   end
-  result
+  return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning(path, emoticon)
-  emoticon_hash = load_library(path)
-  result = emoticon_hash["get_meaning"][emoticon]
-  if result == nil
-    puts result = "Sorry, that emoticon was not found" 
+def get_english_meaning(file_path, emoticon)
+  emoticons = load_library(file_path)
+  emoticons["get_meaning"].each do |japanese_emoticon, english_meaning|
+    if emoticon == japanese_emoticon
+      return english_meaning
+    end
   end
-  result
+  return "Sorry, that emoticon was not found"
 end

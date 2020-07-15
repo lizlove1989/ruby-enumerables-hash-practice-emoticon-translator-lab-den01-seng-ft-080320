116a1e8221dc77b33221f909950f76661b48e490
Liz Love <love.elizabeth1989@gmail.com>
2020-07-15 22:04:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,13 +5,13 @@ def load_library(file_path)
   emoticons = YAML.load_file(file_path)
 
   return_hash = {
-    "get_meaning" => {},
-    "get_emoticon" => {},
+    get_meaning: => {},
+    get_emoticon: => {},
   }
 
   emoticons.each do |meaning, translations|
-    return_hash["get_meaning"][translations[1]] = meaning
-    return_hash["get_emoticon"][translations[0]] = translations[1]
+    return_hash[get_meaning][translations[1]] = meaning
+    return_hash[get_emoticon][translations[0]] = translations[1]
   end
   return_hash
 end

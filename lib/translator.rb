51ba5fc118ef2b7cd70bbbac59767f2f429b9804
Liz Love <love.elizabeth1989@gmail.com>
2020-07-15 22:02:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(file_path)
   emoticons = YAML.load_file(file_path)
 
   return_hash = {
-    "get_meaning" => {},
+   :meaning, "get_meaning" => {},
     "get_emoticon" => {},
   }
 

fdde4699841f83f6a2b640d3b270d668509f9bc2
Liz Love <love.elizabeth1989@gmail.com>
2020-07-15 21:53:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ end
 def get_japanese_emoticon(path, emoticon)
   emoticon_hash = load_library(path)
   result = emoticon_hash["get_emoticon"][emoticon]
-  if result == nil
+  if result == :japanese
     result = "Sorry, that emoticon was not found" 
   end
   result
